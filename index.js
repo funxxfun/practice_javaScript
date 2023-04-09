2a2e074f3604af845f84bfd14982a249cea3a77d
Fumi <23shirasawa@gmail.com>
2023-04-09 17:50:40 +0900

    関数の共通化

--- index.js
+++ index.js
@@ -1,78 +1,93 @@
+const completeListGroup = document.getElementById('complete-list-group');
 const incompleteListGroup = document.getElementById('incomplete-list-group');
 const addTasks = document.getElementById('input-area');
 const addButton = document.getElementById('add-button');
-const completeListGroup = document.getElementById('complete-list-group');
 
 // 追加するボタンを押したら未完了ToDoが追加される
 addButton.addEventListener('click', function(event){
   event.preventDefault();
-  addIncompleteList();
+  createIncompleteList(addTasks.value);
+  addTasks.value = "";
 });
 
-// 追加ボタンを押した時に実行される関数
-function addIncompleteList() {
-  // liタグが生成されてタスクが表示される
-  const incompleteListItem = document.createElement('li');
-  incompleteListItem.classList.add('incomplete-list-item');
-  incompleteListItem.innerText = addTasks.value;
-  incompleteListGroup.appendChild(incompleteListItem);
-  addTasks.value = "";
+// タスクを削除する部分の共通化
+const deleteFromIncompleteList = function(target) {
+  document.getElementById('incomplete-list-group').removeChild(target);
+}
 
-  // 左クリックで削除線
-  // incompleteListItem.addEventListener('click', function() {
-  //   incompleteListItem.classList.toggle('text-decoration-line-through');
-  // });
+// 未完了エリアにタスクが追加される部分共通化
+const createIncompleteList = function(text) {
+  // divタグが生成される
+  const div = document.createElement('div');
+  div.className = "row";
+  // divタグの中にliタグが生成されてタスクが表示される
+  const li = document.createElement('li');
+  li.innerText = text;
+  li.className = "col";
 
-
-  // liタグの中に完了ボタンが生成される
+  // divタグの中に完了ボタンが生成される
   const completeButton = document.createElement('button');
   completeButton.innerText = "完了";
-  incompleteListItem.appendChild(completeButton);
   completeButton.setAttribute('type', 'button');
-  completeButton.setAttribute('class', 'btn btn-success m-1');
-
-
+  completeButton.setAttribute('class', 'btn btn-success m-1 col-3');
   //  完了ボタンを押したら完了エリアにタスクが移動する
-completeButton.addEventListener('click', function(event){
-  event.preventDefault();
-  moveCompleteList();
-});
-
-// 完了ボタンを押した時に実行される関数
-function moveCompleteList() {
-    const completeTarget = completeButton.parentNode;
-    // 未完了エリアから削除される
-    incompleteListGroup.removeChild(completeTarget);
-    // 完了エリアにliダグが生成される
-    const completeListItem = document.createElement('li');
-    completeListItem.classList.add('complete-list-item');
-    completeListGroup.appendChild(completeListItem);
-
-    // 完了ボタンを押したinnerTextのvalueを取得するに変更
-    completeListItem.innerText = incompleteListItem.value;
-
-    // console.log(incompleteListItem);
-    console.log(incompleteListItem.innerHTML);
-
-  };
-
-  // liタグの中に削除ボタンを生成する
-  const deleteButton = document.createElement('button');
-  deleteButton.innerText = "削除する";
-  incompleteListItem.appendChild(deleteButton);
-  deleteButton.setAttribute('type', 'button');
-  deleteButton.setAttribute('class', 'btn btn btn-warning');
-
-  // 削除ボタンを押したら削除イベントが発動
-  const deleteTasks = deleteButton.addEventListener('click', function(event){
+  completeButton.addEventListener('click', function(event){
     event.preventDefault();
-    alert('削除しますか？');
-    const deleteTarget = deleteButton.parentNode;
-    incompleteListGroup.removeChild(deleteTarget);
+    // 未完了エリアから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+    // 完了エリアにdivタグが生成される
+    const div = document.createElement('div');
+    div.className = "row";
+    // divタグの中にliタグが生成されてタスクが表示される
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    // 完了ボタンの親ノードないのテキストをからにしてやる
+    // 削除ボタンと完了ボタンを削除するために
+    addTarget.textContent = null;
+    const li = document.createElement('li');
+    li.innerText = text;
+    li.className = "col";
+    
+    // 完了エリアのdivタグの中に戻すボタンが生成される
+    const reverseButton = document.createElement('button');
+    reverseButton.innerText = "戻す";
+    reverseButton.setAttribute('type', 'button');
+    reverseButton.setAttribute('class', 'btn btn-outline-secondary m-1 col-3');
+
+    //  戻すボタンを押したら未完了エリアにタスクが移動する
+    reverseButton.addEventListener('click', function(event){
+      event.preventDefault();
+      const reverseTarget = reverseButton.parentNode;
+      // 完了エリアから削除される
+      completeListGroup.removeChild(reverseTarget);
+      
+      // 完了ボタンを押したリストの値を取得する
+      const text = reverseTarget.firstElementChild.innerText;
+      createIncompleteList(text); 
+    });
+    addTarget.appendChild(li);
+    addTarget.appendChild(reverseButton);
+    completeListGroup.appendChild(addTarget);
   });
 
-};
+  // divタグの中に削除ボタンが生成される
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = "削除する";
+  deleteButton.setAttribute('type', 'button');
+  deleteButton.setAttribute('class', 'btn btn btn-warning m-1 col-3');
 
+  // 削除ボタンを押したら削除イベントが発動
+  const deleteTasks = deleteButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('削除しますか？');
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+    div.appendChild(li);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+    incompleteListGroup.appendChild(div);  
+};
 
 
 
